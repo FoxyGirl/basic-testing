94ebda6d446b109b616a3a734fac76177d69cec5
Elena Pavlova <lenlho22@gmail.com>
2024-10-20 11:30:41 +0300

    feat: implement error handling & async tests

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,49 @@
 // Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    const testValue = 'Test value';
+    const val = await resolveValue(testValue);
+    expect(val).toBe(testValue);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
     // Write your test here
+    const errMessage = 'Crash!';
+
+    expect(() => throwError(errMessage)).toThrow(errMessage);
   });
 
   test('should throw error with default message if message is not provided', () => {
     // Write your test here
+    expect(() => throwError()).toThrow('Oops!');
   });
 });
+
+const defaultAwesomeErrorMessage = 'This is my awesome custom error!';
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
     // Write your test here
+    expect(throwCustomError).toThrow(MyAwesomeError);
+
+    expect(throwCustomError).toThrow(defaultAwesomeErrorMessage);
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
     // Write your test here
+    await expect(rejectCustomError).rejects.toThrow(MyAwesomeError);
+    await expect(rejectCustomError).rejects.toThrow(defaultAwesomeErrorMessage);
   });
 });
